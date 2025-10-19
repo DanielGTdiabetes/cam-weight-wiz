4655f67a079907000b6b29519c415172b5eea8f5
DanielGTdiabetes <danieltellols@gmail.com>
2025-10-19 14:00:32 +0200

    Use backend camera capture by default

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // Se sustituye en build por install-all.sh
-const CACHE_VERSION = "cwz-0.0.1-6e27b46";
+const CACHE_VERSION = "cwz-0.0.1-3c46236";
 const STATIC_CACHE = `bascula-static-${CACHE_VERSION}`;
 const PRECACHE_ASSETS = [
   '/manifest.json',
